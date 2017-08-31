2a04142b6ff2b0b574fd48d1c24225db4e52309a
Bailey <bhsostek@wpi.edu>
2017-08-31 17:01:53 -0400

    First Test

---
+++ src/Base/Main.java
@@ -0,0 +1,10 @@
+package Base;
+
+/**
+ * Created by Bailey on 8/31/2017.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Edaxerum is cool!");
+    }
+}
